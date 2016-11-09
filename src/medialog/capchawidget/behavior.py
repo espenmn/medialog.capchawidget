13d4b58b3c1693c70460781a7e0083703c13782b
Espen Moe-Nilssen <espen@medialog.no>
2016-11-09 16:20:57 +0100

    try validation

--- src/medialog/capchawidget/behavior.py
+++ src/medialog/capchawidget/behavior.py
@@ -6,33 +6,33 @@ from plone.autoform.interfaces import IFormFieldProvider
 from zope.interface import alsoProvides
 from zope.i18nmessageid import MessageFactory
 
-from zope.interface import invariant, Invalid
+#from zope.interface import invariant, Invalid
+from zope.interface import Invalid
+from z3c.form import validator
 
 from medialog.capchawidget.widgets.widget import CapchaFieldWidget
 
 _ = MessageFactory('medialog.capchawidget')
 
     
-    
-
-def checkForMagic(value):
-   return 'magic' in value
+#def checkForMagic(value):
+#   return 'magic' in value
    
    
-def verify(self, input=None):
-    info = IRecaptchaInfo(self.request)
-    if info.verified:
-        return True
+def capcha_verify(self, input=None):
+    #info = IRecaptchaInfo(self.request)
+    #if info.verified:
+    #    return True
 
-    if not self.settings.private_key:
-        raise ValueError(
-            'No recaptcha private key configured. '
-            'Go to /@@recaptcha-settings to configure.'
-        )
+    #if not self.settings.private_key:
+    #    raise ValueError(
+    #        'No recaptcha private key configured. '
+    #        'Go to /@@recaptcha-settings to configure.'
+    #    )
+    import pdb; pdb.set_trace()
     response_field = self.request.get('g-recaptcha-response')
-    remote_addr = self.request.get('HTTP_X_FORWARDED_FOR', '').split(',')[0]
-    if not remote_addr:
-        remote_addr = self.request.get('REMOTE_ADDR')
+    remote_addr = 'https://www.google.com/recaptcha/api/siteverify'
+    key = self.portal_registry['medialog.capchawidget.interfaces.ICapchaSettings.key']
     res = submit(response_field, self.settings.private_key, remote_addr)
     if res.error_code:
         info.error = res.error_code
@@ -48,7 +48,7 @@ class ICapchaBehavior(form.Schema):
     
     capchafield = schema.TextLine(
         title = _("capcha", default=u"Capcha"),
-        constraint=checkForMagic,
+        constraint=capcha_verify,
         required = False,
         description = _("help_capcha",
                       default="Dont be a robot"),
