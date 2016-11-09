239f82f261e842b4fc3b7c92aad3ac73479acb3e
Espen Moe-Nilssen <espen@medialog.no>
2016-11-09 16:34:11 +0100

    same

--- src/medialog/capchawidget/behavior.py
+++ src/medialog/capchawidget/behavior.py
@@ -14,34 +14,33 @@ from medialog.capchawidget.widgets.widget import CapchaFieldWidget
 
 _ = MessageFactory('medialog.capchawidget')
 
-    
-#def checkForMagic(value):
-#   return 'magic' in value
-   
-   
-def capcha_verify(self, input=None):
-    #info = IRecaptchaInfo(self.request)
-    #if info.verified:
-    #    return True
-
-    #if not self.settings.private_key:
-    #    raise ValueError(
-    #        'No recaptcha private key configured. '
-    #        'Go to /@@recaptcha-settings to configure.'
-    #    )
-    import pdb; pdb.set_trace()
-    response_field = self.request.get('g-recaptcha-response')
-    remote_addr = 'https://www.google.com/recaptcha/api/siteverify'
-    key = self.portal_registry['medialog.capchawidget.interfaces.ICapchaSettings.key']
-    res = submit(response_field, self.settings.private_key, remote_addr)
-    if res.error_code:
-        info.error = res.error_code
-
-    info.verified = res.is_valid
-    return res.is_valid
 
 
+class CapchaValidator(validator.SimpleFieldValidator):
+    """ z3c.form validator class for international phone numbers """
 
+    def validate(self, value):
+        #info = IRecaptchaInfo(self.request)
+        #if info.verified:
+        #    return True
+
+        #if not self.settings.private_key:
+        #    raise ValueError(
+        #        'No recaptcha private key configured. '
+        #        'Go to /@@recaptcha-settings to configure.'
+        #    )
+        import pdb; pdb.set_trace()
+        response_field = self.context.request.get('g-recaptcha-response')
+        remote_addr = 'https://www.google.com/recaptcha/api/siteverify'
+        key = self.portal_registry['medialog.capchawidget.interfaces.ICapchaSettings.key']
+        res = submit(response_field, self.settings.private_key, remote_addr)
+        if res.error_code:
+            info.error = res.error_code
+
+        info.verified = res.is_valid
+        return res.is_valid
+
+@form.validator.validator(field=ICapchaBehavior['capchafield'])
 class ICapchaBehavior(form.Schema):
     """ A field for capcha"""
     
