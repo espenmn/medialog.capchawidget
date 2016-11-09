42ee947c1dca2374b2e9793b21ddca7d1bf00ee9
Espen Moe-Nilssen <espen@medialog.no>
2016-11-09 16:04:17 +0100

    fix indention

--- src/medialog/capchawidget/behavior.py
+++ src/medialog/capchawidget/behavior.py
@@ -14,31 +14,31 @@ _ = MessageFactory('medialog.capchawidget')
 
     
     
-    
-    def checkForMagic(value):
-       return 'magic' in value
-       
-       
-    def verify(self, input=None):
-        info = IRecaptchaInfo(self.request)
-        if info.verified:
-            return True
 
-        if not self.settings.private_key:
-            raise ValueError(
-                'No recaptcha private key configured. '
-                'Go to /@@recaptcha-settings to configure.'
-            )
-        response_field = self.request.get('g-recaptcha-response')
-        remote_addr = self.request.get('HTTP_X_FORWARDED_FOR', '').split(',')[0]
-        if not remote_addr:
-            remote_addr = self.request.get('REMOTE_ADDR')
-        res = submit(response_field, self.settings.private_key, remote_addr)
-        if res.error_code:
-            info.error = res.error_code
+def checkForMagic(value):
+   return 'magic' in value
+   
+   
+def verify(self, input=None):
+    info = IRecaptchaInfo(self.request)
+    if info.verified:
+        return True
 
-        info.verified = res.is_valid
-        return res.is_valid
+    if not self.settings.private_key:
+        raise ValueError(
+            'No recaptcha private key configured. '
+            'Go to /@@recaptcha-settings to configure.'
+        )
+    response_field = self.request.get('g-recaptcha-response')
+    remote_addr = self.request.get('HTTP_X_FORWARDED_FOR', '').split(',')[0]
+    if not remote_addr:
+        remote_addr = self.request.get('REMOTE_ADDR')
+    res = submit(response_field, self.settings.private_key, remote_addr)
+    if res.error_code:
+        info.error = res.error_code
+
+    info.verified = res.is_valid
+    return res.is_valid
 
 
 
