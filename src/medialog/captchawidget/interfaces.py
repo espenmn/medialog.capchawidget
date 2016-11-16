4b7ed44746dd8e68b40c3e6606f76f89a2fcb3e3
Espen Moe-Nilssen <espen@medialog.no>
2016-11-16 19:43:40 +0100

    giving up fields to make it work

--- src/medialog/captchawidget/interfaces.py
+++ src/medialog/captchawidget/interfaces.py
@@ -8,7 +8,10 @@ from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 from zope.interface import alsoProvides
 from plone.directives import form
-#from collective.recaptcha.settings  import IRecaptchaSettings
+
+
+from collective.recaptcha.settings  import IRecaptchaSettings
+
 from medialog.controlpanel.interfaces import IMedialogControlpanelSettingsProvider
 
 
@@ -16,26 +19,26 @@ class IMedialogCaptchawidgetLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
 
 
-class IRecaptchaSettings(form.Schema):
-    """Your google captcha keys
-    """
-
-    form.fieldset(
-        'captcha',
-        label=_(u'Captcha'),
-        fields=[
-             'private_key',
-             'public_key',
-            ],
-     )
-
-    private_key = schema.TextLine (
-    	title=_(u"label_key", default=u"Site Key"),
-    )
-
-    public_key = schema.TextLine (
-    	title=_(u"label_secret", default=u"Secret Key"),
-    )
+#class IWRecaptchaSettings(form.Schema):
+#    """Your google captcha keys
+#    """
+#
+#    form.fieldset(
+#        'captcha',
+#        label=_(u'Captcha'),
+#        fields=[
+#             'private_key',
+#             'public_key',
+#            ],
+#     )
+#
+#    private_key = schema.TextLine (
+#    	title=_(u"label_key", default=u"Site Key"),
+#    )
+#
+#    public_key = schema.TextLine (
+#    	title=_(u"label_secret", default=u"Secret Key"),
+#    )
 
         
 alsoProvides(IRecaptchaSettings, IMedialogControlpanelSettingsProvider)
